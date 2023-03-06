7a8decc9d9ab30096f27fec10477c60f1f917a32
thanghoian123 <57632755+thanghoian123@users.noreply.github.com>
2023-03-06 15:06:48 +0700

    Update element.sh

--- element.sh
+++ element.sh
@@ -11,10 +11,10 @@ fi
 #if argument is atomic number
 if [[ $1 =~ ^[1-9]+$ ]]
 then
-  element=$($PSQL "select atomic_number, name, symbol, properties.type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements join properties using(atomic_number) join types using(type_id) where atomic_number = '$1'")
+  element=$($PSQL "select atomic_number, name, symbol, types.type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements join properties using(atomic_number) join types using(type_id) where atomic_number = '$1'")
 else
 #if argument is string
-  element=$($PSQL "select atomic_number, name, symbol, properties.type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements join properties using(atomic_number) join types using(type_id) where name = '$1' or symbol = '$1'")
+  element=$($PSQL "select atomic_number, name, symbol, types.type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements join properties using(atomic_number) join types using(type_id) where name = '$1' or symbol = '$1'")
 fi
 
 #element not in db
